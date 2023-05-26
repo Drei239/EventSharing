c5e32fbadd39633331f953ad5e2ef84c35d38d49
haolk003 <nguyenquochaolop91@gmail.com>
2023-05-26 12:53:00 +0700

    add

---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as CardEvent } from "./cardEvent/cardEvent";
+export { default as Carousel } from "./carousel/index";
+export { default as CardOrg } from "./cardOrg/cardOrganizers";
