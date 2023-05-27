befb356114e27d6bf174cd63fd408f658f6d139f
KaidouSea <39830574+KaidouSea@users.noreply.github.com>
2023-05-27 10:46:53 +0700

    Add files via upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,10 @@ const asyncHandler = require('express-async-handler');
 const userModel = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 
+
+const tokenTime = require('../utils/tokenTime');
+const userModel = require('../models/userModel');
+
 //1.GET ALL USER INFO
 const getAllUser = asyncHandler(async (req, res) => {
     const users = await userModel.find({});
@@ -9,7 +13,29 @@ const getAllUser = asyncHandler(async (req, res) => {
 });
 
 //2.REGISTER NEW USER
+
 const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExists = await userModel.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error('Tài khoản người dùng đã tồn tại');
+    }
+    const newUser = await userModel.create({ name, email, password });
+    if (newUser) {
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+        token: tokenTime(newUser._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error('Dữ liệu nhập không hợp lệ');
+    }
+  });
+`const registerUser = asyncHandler(async (req, res) => {
     const { name, reviews } = req.body
     const newUser = await userModel.create({ name, reviews });
     if (newUser) {
@@ -23,9 +49,10 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Invalid user data!');
     }
-});
+});`
 
 //3.GET USER INFO BY ID
+
 const getUserProfile = asyncHandler(async (req, res) => {
     const user = await userModel.findById(req.params.id);
     if (user) {
