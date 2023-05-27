df328203b2021080603bef02f1470a61f79db812
DESKTOP-ORRN0CM\Toan Dang <toandang2309@gmail.com>
2023-05-27 19:25:51 +0700

    DEMO MIN 11

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -3,18 +3,24 @@ const eventModel = require("../models/eventModel");
 
 //1.CREATE NEW EVENT
 const createNewEvent = asyncHandler(async (req, res) => {
-    let { title, timeEndSignup, timeBegin, timeEnd } = req.body;
+    const { title, description, banner, imageList,
+        category, type, fee, location,
+        timeEndSignup, timeBegin, timeEnd, creator,
+        limitUser, reviews } = req.body;
+    // Sau khi gán userInfo = req.user 
+    // const creator = req.user.id;
+    // loại bỏ giá trị creator ở req.body
     if (Date.parse(timeEndSignup) < Date.parse(timeBegin) &&
         Date.parse(timeEndSignup) < Date.parse(timeEnd) &&
         Date.parse(timeBegin) < Date.parse(timeEnd)) {
-        const event = await eventModel.create({
-            title,
-            timeEndSignup,
-            timeBegin,
-            timeEnd
+        const newEvent = await eventModel.create({
+            title, description, banner, imageList,
+            category, type, fee, location,
+            timeEndSignup, timeBegin, timeEnd, creator,
+            limitUser, reviews
         });
-        if (event) {
-            res.status(200).json(event);
+        if (newEvent) {
+            res.status(200).json(newEvent);
         } else {
             res.status(401);
             throw new Error("CREATE NEW EVENT FAILED!");
@@ -25,4 +31,45 @@ const createNewEvent = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createNewEvent }
+//2.GET ALL EVENT - CHỈ HIỂN THỊ CÁC EVENT ĐANG CÓ STATUS PUBLIC
+//"$and"[{ "status": "Draft" }, { "status": "Public" }]
+//DANH SÁCH THEO EVENT RATING GIẢM DẦN find().sort({eventRating:-1}).limit(1)
+//EVENT RATING TĂNG DẦN find().sort({eventRating:+1}).limit(1)
+const getAllEvent = asyncHandler(async (req, res) => {
+    const events = await eventModel.find({ "status": "Public" });
+    if (events) {
+        res.status(200).json(events);
+    } else {
+        res.status(401);
+        throw new Error("KHÔNG TÌM THẤY BẤT CỨ EVENT NÀO!");
+    }
+});
+
+//3.GET INFO EVENT BY ID
+const getEventById = asyncHandler(async (req, res) => {
+    const event = await eventModel.find({ _id: req.params.id });
+    if (event) {
+        res.status(200).json({ event });
+    } else {
+        res.status(401);
+        throw new Error("KHÔNG TÌM THẤY EVENT!");
+    }
+});
+
+//4.GET EVENT BY CREATOR
+//CẦN KIỂM TRA REQUEST USER = CREATOR OR GUEST ĐỂ PHÂN LOẠI HIỂN THỊ
+//STATUS = "?" DRAFT PENDING PUBLIC COMPLETED CANCELED
+const getEventByCreator = asyncHandler(async (req, res) => {
+    const event = await eventModel.find({ creator: req.params.id });
+    if (event) {
+        res.status(200).json(event);
+    } else {
+        res.status(401);
+        throw new Error("KHÔNG TÌM THẤY EVENT CỦA NGƯỜI DÙNG!");
+    }
+})
+
+module.exports = {
+    createNewEvent, getAllEvent,
+    getEventById, getEventByCreator
+}
--- server/routes/events.js
+++ server/routes/events.js
@@ -1,8 +1,23 @@
 let express = require("express");
 let router = express.Router();
-const { createNewEvent } = require("../controllers/eventController");
+const { createNewEvent, getAllEvent,
+    getEventById, getEventByCreator } = require("../controllers/eventController");
 
 //1.CREATE NEW EVENT
 router.post("/create", createNewEvent);
+
+//2.GET ALL EVENT STAUS = "PUBLIC"
+router.get("/all", getAllEvent);
+
+//3.GET EVENT BY ID
+//PUBLIC CHO NGƯỜI SỬ DỤNG XEM CHI TIẾT CÁC THÔNG TIN VỀ 1 SỰ KIỆN
+router.get("/:id", getEventById);
+
+//4.GET EVENT BY CREATOR
+//PUBLIC CHO NGƯỜI SỬ DỤNG XEM THÔNG TIN VỀ CÁC SỰ KIỆN CỦA 1 NGƯỜI TCSK
+//LẤY DANH SÁCH SỰ KIỆN CỦA 1 USER ĐỂ HIỂN THỊ TRÊN PROFILE?
+//NẾU REQUEST USER = CREATOR -> GET FULL EVENT (BAO GỒM DRAFT, PENDING)
+//NẾU REQUEST USER = GUEST -> DON'T GET DRAFT, PENDING
+router.get("/user/:id", getEventByCreator);
 
 module.exports = router;
