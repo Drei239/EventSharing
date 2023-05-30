4804756e71d8692e7eeb6cc5d6e4b49bf6e5d132
jackie <lazmonbiro@gmail.com>
2023-05-30 20:06:49 +0700

    fix Link

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -6,34 +6,34 @@ const Header = () => {
 	return (
 		<div className='wrapper'>
 			<header>
-					<div className="header__left-block">
-						<Link className="logo" href='/' alt=''>
-							<img src={"./images/logo.png"} />
-						</Link>
-						<div className="header__search">
-							<Input width='290px' placeholder="Search" />
-						</div>
-						<div className="header__category">
-							<div class="dropdown">
-								<Link class="dropdown__catergory" href='/' alt=''>Sự kiện</Link>
+				<div className="header__left-block">
+					<Link className="logo" href='/' alt=''>
+						<img src={"./images/logo.png"} />
+					</Link>
+					<div className="header__search">
+						<Input width='290px' placeholder="Search" />
+					</div>
+					<div className="header__category">
+						<div class="dropdown">
+							<Link class="dropdown__catergory" href='/' alt=''>Sự kiện</Link>
 							<div class="dropdown__content">
-									<a href='/'>Link 1</a>
-									<a href='/'>Link 2</a>
-									<a href='/'>Link 3</a>
-								</div>
+								<Link href='/'><div className='category__item'>Link 1</div></Link>
+								<Link href='/'><div className='category__item'>Link 2</div></Link>
+								<Link href='/'><div className='category__item'>Link 3</div></Link>
 							</div>
 						</div>
 					</div>
-					<div className="header__right-block">
-						<div className="create__event">
-							<a className="header_btn header__btn-create" href='/' alt=''>Tạo sự kiện</a>
-							<img src="" alt="" />
-						</div>
-						<div className='header__log'>
-							<a className="header_btn" href='/' alt=''>Đăng ký</a>|
-							<a className="header_btn" href='/' alt=''>Đăng nhập</a>
-						</div>
+				</div>
+				<div className="header__right-block">
+					<div className="create__event">
+						<Link href='/' alt=''><div className="header_btn header__btn-create">Tạo sự kiện</div></Link>
+						<img src="" alt="" />
 					</div>
+					<div className='header__log'>
+						<Link href='/' alt=''><div className="header_btn">Đăng ký</div></Link>|
+						<Link href='/' alt=''><div className="header_btn">Đăng nhập</div></Link>
+					</div>
+				</div>
 			</header>
 		</div>
 	);
