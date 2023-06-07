b751ad45b07c3e62092c2ed00d20100f0fdc6eb5
DESKTOP-ORRN0CM\Toan Dang <toandang2309@gmail.com>
2023-06-07 09:49:38 +0700

    CREATE BRANCH MIN-11 FOR API CREATE/GET EVENTS

---
+++ server/validators/responsiveMessages.js
@@ -0,0 +1,13 @@
+const eventError = {
+    ERR_1: "TẠO SƯ KIỆN MỚI THẤT BẠI!",
+    ERR_2: "KHÔNG TÌM THẤY SỰ KIỆN PUBLIC NÀO!",
+    ERR_3: "KHÔNG TÌM THẤY EVENT",
+}
+
+const eventSucc = {
+    SUC_1: "TẠO SỰ KIỆN MỚI THÀNH CÔNG!",
+    SUC_2: "GET SỰ KIỆN PUBLIC THÀNH CÔNG!",
+    SUC_3: "GET THÀNH CÔNG SỰ KIỆN"
+}
+
+module.exports = { eventError, eventSucc }
