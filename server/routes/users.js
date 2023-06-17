e1461e4dfee090e0ecb16780453bdbbb406b5de4
haolk003 <nguyenquochaolop91@gmail.com>
2023-06-17 09:13:20 +0700

    🐞 fix:

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const {
@@ -11,25 +11,25 @@ const {
   checkAccount,
   logout,
   deleted,
-} = require('../controllers/userController');
-const { protect, isAdmin } = require('../middleware/authMiddleware');
+} = require("../controllers/userController");
+const { protect, isAdmin } = require("../middleware/authMiddleware");
 const {
   registerValidate,
   loginValidate,
   updateUserValidate,
-} = require('../middleware/validate');
-const { ref } = require('joi');
+} = require("../middleware/validate");
+const { ref } = require("joi");
 
-router.get('/profile', protect, profileUser);
-router.get('/getall', protect, isAdmin, getAllUser);
-router.post('/login', authLogin);
-router.post('/register', registerValidate, register);
-router.post('/check', checkAccount);
-router.get('/logout', logout);
-router.post('/update/:id', protect, updateUserValidate, updateUserById);
-router.delete('/admin/:id', protect, isAdmin, deleted);
+router.get("/profile", protect, profileUser);
+router.get("/getall", protect, isAdmin, getAllUser);
+router.post("/login", authLogin);
+router.post("/register", registerValidate, register);
+router.post("/check", checkAccount);
+router.get("/logout", logout);
+router.post("/update/:id", protect, updateUserValidate, updateUserById);
+router.delete("/admin/:id", protect, isAdmin, deleted);
 
-router.get("/account", getUserPersonal);
+// router.get("/account", getUserPersonal);
 
-router.get("update", updateUser);
+// router.get("update", updateUser);
 module.exports = router;
