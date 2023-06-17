45a17725af0994f5d5a41c19c62198c7b558c696
Gia Hao <pgiahao11@gmail.com>
2023-06-17 19:47:18 +0700

    Fix error merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
-import LoginRegisterPage from "./pages/login-register/LoginRegisterPage";
-import CreateEventPage from "./pages/create-event/CreateEventPage";
-import { Divider, NextUIProvider } from "@nextui-org/react";
-import { Home, Events, Setting } from "./pages";
-import { useSelector } from "react-redux";
-import { NextUIProvider } from "@nextui-org/react";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import LoginRegisterPage from './pages/login-register/LoginRegisterPage';
+import CreateEventPage from './pages/create-event/CreateEventPage';
+import { Home, Events, Setting } from './pages';
+import { useSelector } from 'react-redux';
+import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
   const open = useSelector((state) => state.user.open);
   return (
-    <div className={open && "hidden-scroll"}>
+    <div className={open && 'hidden-scroll'}>
       <NextUIProvider>
         <Header />
         <Routes>
-          <Route path="/login-register" element={<LoginRegisterPage />} />
-          <Route path="/create-event" element={<CreateEventPage />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/account/*" element={<Setting />} />
+          <Route path='/login-register' element={<LoginRegisterPage />} />
+          <Route path='/create-event' element={<CreateEventPage />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/events' element={<Events />} />
+          <Route path='/account/*' element={<Setting />} />
         </Routes>
         <Footer />
       </NextUIProvider>
--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,10 +1,18 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, createAction } from '@reduxjs/toolkit';
 import customFetch from '../../utils/axios.config';
+export const openModal = createAction('openModal');
+export const closeModal = createAction('closeModal');
+export const saveImg = createAction('selectImg', function prepare(img) {
+  return { payload: img };
+});
+export const removeImg = createAction('remove-Img');
 
 const initialState = {
   userInfo: {},
   isLoading: true,
   isLogin: false,
+  open: false,
+  imgSelect: null,
 };
 
 export const getUserInfo = createAsyncThunk(
@@ -45,6 +53,20 @@ const userSlice = createSlice({
         state.isLoading = false;
         state.isLogin = false;
       });
+
+    builder.addCase(openModal, (state) => {
+      state.open = true;
+    });
+    builder.addCase(closeModal, (state) => {
+      state.open = false;
+    });
+    builder.addCase(saveImg, (state, action) => {
+      state.imgSelect = action.payload;
+      state.open = false;
+    });
+    builder.addCase(removeImg, (state) => {
+      state.imgSelect = null;
+    });
   },
 });
 
