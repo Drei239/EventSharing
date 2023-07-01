6324952ca8c1e85cdb3f0a0bffd9a0080fec97f9
DESKTOP-ORRN0CM\Toan Dang <toandang2309@gmail.com>
2023-07-01 17:49:09 +0700

    UPDATE CREATE REPLY COMMENT

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
+const replySchema = mongoose.Schema({
+    title: { type: String, required: true },
+    comment: { type: String, required: true },
+    creator: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+    createdAt: { type: Date, default: Date.now },
+});
+
 const commentSchema = mongoose.Schema({
     title: { type: String, required: true },
     comment: { type: String, required: true },
     event: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Event" },
     creator: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    reply: [replySchema]
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
