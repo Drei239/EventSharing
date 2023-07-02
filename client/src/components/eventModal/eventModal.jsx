439c08c78ec48ae8b17297af72387106f9b38f82
jackie <lazmonbiro@gmail.com>
2023-07-02 14:45:22 +0700

    buy button

--- client/src/components/eventModal/eventModal.jsx
+++ client/src/components/eventModal/eventModal.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Modal, useModal, Text, Link } from '@nextui-org/react';
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getOrderbyId } from "../../features/order/orderSlice";
 import './eventModal.css';
 // import { FaListAlt } from "react-icons/fa";
 
 const EventModal = () => {
+
+	const { orders } = useSelector((state) => state.order);
 	const { setVisible, bindings } = useModal();
+	const { id } = useParams();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getOrderbyId({ id, status: "all" }));
+	}, []);
+
+	console.log(orders);
+
 	return (
 		<div>
 			<button className='members__list' onClick={() => setVisible(true)}>
-				So nguoi tham gia
+				So nguoi tham gia: {orders?.length}
 			</button>
 			<Modal
 				scroll
@@ -23,60 +37,17 @@ const EventModal = () => {
 					</Text>
 				</Modal.Header>
 				<div className="participants__container">
-						<Link>
-							<div className="participants">
-								<div className="user__info">
-									<img className="user__avatar" src="https://images3.alphacoders.com/654/654249.png" alt="" />
-									<div className="user__name">name</div>
+					<Link>
+						<div className="participants">
+							<div className="user__info">
+								<img className="user__avatar" src="https://images3.alphacoders.com/654/654249.png" alt="" />
+								<div className="user__name">
+									{/* {orders?.map((item) => item.user.name)} */}
 								</div>
-								<div className="user__status">status</div>
 							</div>
-						</Link>
-					<div>
-						<Link>
-							<div className="participants">
-								<div className="user__info">
-									<img className="user__avatar" src="https://images3.alphacoders.com/654/654249.png" alt="" />
-									<div className="user__name">name</div>
-								</div>
-								<div className="user__status">status</div>
-							</div>
-						</Link>
-					</div>
-					<div>
-						<Link>
-							<div className="participants">
-								<div className="user__info">
-									<img className="user__avatar" src="https://images3.alphacoders.com/654/654249.png" alt="" />
-									<div className="user__name">name</div>
-								</div>
-								<div className="user__status">status</div>
-							</div>
-						</Link>
-					</div>
-					<div>
-						<Link>
-							<div className="participants">
-								<div className="user__info">
-									<img className="user__avatar" src="https://images3.alphacoders.com/654/654249.png" alt="" />
-									<div className="user__name">name</div>
-								</div>
-								<div className="user__status">status</div>
-							</div>
-						</Link>
-					</div>
-					<div>
-						<Link>
-							<div className="participants">
-								<div className="user__info">
-									<img className="user__avatar" src="https://images3.alphacoders.com/654/654249.png" alt="" />
-									<div className="user__name">name</div>
-								</div>
-								<div className="user__status">status</div>
-							</div>
-						</Link>
-					</div>
-					
+							<div className="user__status">status</div>
+						</div>
+					</Link>								
 				</div>
 			</Modal>
 		</div>
