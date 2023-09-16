c643360ac89f3f6100675811943463dfccf5a299
haolk003 <nguyenquochaolop91@gmail.com>
2023-09-16 23:45:09 +0700

    add

--- client/src/pages/QRCode/QRCode.jsx
+++ client/src/pages/QRCode/QRCode.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import QrReader from 'modern-react-qr-reader';
-import customFetch from '../../utils/axios.config';
-import notify from '../../utils/notify.js';
-import { Modal } from '@nextui-org/react';
-import './QRCode.css';
+import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+// import QrReader from 'modern-react-qr-reader';
+import customFetch from "../../utils/axios.config";
+import notify from "../../utils/notify.js";
+import { Modal } from "@nextui-org/react";
+import "./QRCode.css";
 
 const QRCode = () => {
   const navigate = useNavigate(-1);
@@ -22,10 +22,10 @@ const QRCode = () => {
   useEffect(() => {
     if (qrCode) {
       customFetch
-        .get(`/orders/${qrCode}/${searchParams.get('id')}/checkQrcode`)
+        .get(`/orders/${qrCode}/${searchParams.get("id")}/checkQrcode`)
         .then((resp) => setOrder(resp.data))
         .catch((err) => {
-          notify(`${err.response.data}`, 'error');
+          notify(`${err.response.data}`, "error");
         });
     }
   }, [qrCode]);
@@ -42,7 +42,7 @@ const QRCode = () => {
     if (scanData) {
       setQrCode((prev) => {
         if (prev === scanData) {
-          notify('QR code đã được quét', 'error');
+          notify("QR code đã được quét", "error");
         }
         return scanData;
       });
@@ -54,63 +54,63 @@ const QRCode = () => {
   };
 
   return (
-    <section className='qrcode'>
+    <section className="qrcode">
       <p onClick={() => navigate(-1)}>
         <svg
-          xmlns='http://www.w3.org/2000/svg'
-          className='icon icon-tabler icon-tabler-chevron-left'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          strokeWidth='2'
-          stroke='currentColor'
-          fill='none'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          style={{ verticalAlign: 'top', color: 'var(--color-primary2)' }}
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-chevron-left"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          style={{ verticalAlign: "top", color: "var(--color-primary2)" }}
         >
-          <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
-          <path d='M15 6l-6 6l6 6'></path>
-        </svg>{' '}
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M15 6l-6 6l6 6"></path>
+        </svg>{" "}
         Quay lại
       </p>
-      <section className='qrcode__scan'>
-        <h3 style={{ textAlign: 'center', marginBottom: '20px' }}>
+      <section className="qrcode__scan">
+        <h3 style={{ textAlign: "center", marginBottom: "20px" }}>
           Quét QR code
         </h3>
-        {startScan && (
+        {/* {startScan && (
           <QrReader
             facingMode={'environment'}
             delay={300}
             onError={handleError}
             onScan={handleScan}
           />
-        )}
+        )} */}
       </section>
 
       <Modal
         closeButton
         blur
-        aria-labelledby='modal-title'
+        aria-labelledby="modal-title"
         open={visible}
         onClose={closeHandler}
-        width='40%'
-        css={{ padding: '30px' }}
+        width="40%"
+        css={{ padding: "30px" }}
       >
         <h3>Thông tin vé</h3>
-        <section className='qrcode__info'>
+        <section className="qrcode__info">
           <p>Mã:</p>
           <p>#{order?._id}</p>
         </section>
-        <section className='qrcode__info'>
+        <section className="qrcode__info">
           <p>Tên sự kiện:</p>
           <p>{order?.event.title}</p>
         </section>
-        <section className='qrcode__info'>
+        <section className="qrcode__info">
           <p>Khách hàng:</p>
           <p>{order?.user.name}</p>
         </section>
-        <section className='qrcode__info'>
+        <section className="qrcode__info">
           <p>Email:</p>
           <p>{order?.user.email}</p>
         </section>
