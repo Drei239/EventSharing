7bb67b578ba5972733a4945838cb93174a4851d1
haolk003 <nguyenquochaolop91@gmail.com>
2023-09-17 00:02:19 +0700

    add

--- client/src/components/ui/index.js
+++ client/src/components/ui/index.js
@@ -1,5 +1,3 @@
 // export { default as Select } from "./select";
-
 export { default as UploadImage } from "./uploadImage";
 export { default as CropImage } from "./cropImage";
-export { default as ExportToExcel } from "./exportToExcel";
